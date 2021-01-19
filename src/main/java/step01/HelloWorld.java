a682b67a92f38ad37f276511f5f7bf393770cd57
= <=>
2021-01-19 18:49:55 +0900

    init

---
+++ src/main/java/step01/HelloWorld.java
@@ -0,0 +1,9 @@
+package step01;
+
+import io.reactivex.rxjava3.core.Flowable;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Flowable.just("Hello World").subscribe(System.out::println);
+    }
+}
